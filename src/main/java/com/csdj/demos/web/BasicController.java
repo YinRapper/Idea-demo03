a2907af053495424f5a6f32fed4e150e4bbe3373
Yin <3174294951@qq.com>
2023-07-06 17:27:24 +0800

    idea-demo01 master third  commit

--- src/main/java/com/csdj/demos/web/BasicController.java
+++ src/main/java/com/csdj/demos/web/BasicController.java
@@ -32,7 +32,7 @@ public class BasicController {
     @RequestMapping("/hello")
     @ResponseBody
     public String hello(@RequestParam(name = "name", defaultValue = "unknown user") String name) {
-        return "Hello world hot-fix  hot-fix提交冲突 Hello world master冲突" + name;
+        return "Hello world hot-fix  hot-fix提交冲突 Hello world master冲突   sss" + name;
     }
 
     // http://127.0.0.1:8080/user
